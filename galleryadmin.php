8d5842272281abd1fa0a427afda169544e2e4093
ZTGaming <62690285+ZTGaming@users.noreply.github.com>
2020-06-19 20:06:01 +0530

    changes for  gallery part

---
+++ galleryadmin.php
@@ -0,0 +1,135 @@
+<?php
+  $_SESSION['username'] = "Admin";
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Global Pride International</title>
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=PT+Sans:wght@700&family=Ubuntu&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="gallery.css">
+  <!-- <link rel="stylesheet" href="zstyles.css"> -->
+  <!-- <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> -->
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <script src="jquery.counterup.min.js" charset="utf-8"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
+  <!-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script> -->
+  <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script src="index.js" charset="utf-8"></script>
+</head>
+
+<body>
+  <section class="container-fluid">
+    <div id=home class="header">
+      <nav>
+        <div class="menu-icon">
+          <i class="fa fa-bars fa-2x"></i>
+        </div>
+        <div class="menu">
+          <ul>
+            <li><a href="index.html">HOME</a></li>
+            <li><a href="#about">ABOUT</a></li>
+            <li><a href="#">ACADEMICS</a></li>
+            <li><a href="#con">CONTACT</a></li>
+            <li><a href="gallery.php">GALLERY</a></li>
+          </ul>
+        </div>
+      </nav>
+      <img class="logo_school animate__animated animate__backInLeft" src="logo.png" alt="" >
+      <h1 id="about" class="animate__animated animate__bounce">Global Pride International School</h1>
+      <h2 class="animate__animated animate__bounce">Gallery</h2>
+
+          <div class="gallery-container container" style="margin-top:400px;margin-left:250px;margin-right:30px;">
+            <?php
+            include_once 'includes/dbh.inc.php';
+
+            $sql = "SELECT * FROM gallery ORDER BY orderGallery DESC;";
+            $stmt = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($stmt, $sql)) {
+              echo "SQL statement failed!";
+            } else {
+              mysqli_stmt_execute($stmt);
+              $result = mysqli_stmt_get_result($stmt);
+
+              while ($row = mysqli_fetch_assoc($result)) {
+                echo '<a href="#">
+                  <div style="background-image: url(img/gallery/'.$row["imgFullNameGallery"].');"></div>
+                  <h3>'.$row["titleGallery"].'</h3>
+                  <p>'.$row["descGallery"].'</p>
+                </a>';
+              }
+            }
+            ?>
+          </div>
+
+          <?php
+          if (isset($_SESSION['username'])) {
+            echo '<div class="gallery-upload">
+              <h2>Upload</h2>
+              <form action="includes/gallery-upload.inc.php" method="post" enctype="multipart/form-data">
+                <input type="text" name="filename" placeholder="File name...">
+                <input type="text" name="filetitle" placeholder="Image title...">
+                <input type="text" name="filedesc" placeholder="Image description...">
+                <input type="file" name="file">
+                <button type="submit" name="submit">UPLOAD</button>
+              </form>
+            </div>';
+          }
+          ?>
+
+
+    <script>
+      AOS.init();
+    </script>
+
+    <script src="jquery.counterup.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+
+    <script>
+      jQuery(document).ready(function($) {
+        $('.counter').counterUp({
+          delay: 10,
+          time: 1000
+        });
+      });
+    </script>
+
+    <!--Script for gallery  -->
+
+    <script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
+    <script>
+      var swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+          rotate: 20,
+          stretch: 0,
+          depth: 200,
+          modifier: 1,
+          slideShadows: true,
+        },
+        loop: true,
+        autoplay: {
+          delay: 500,
+          disableOnInteraction: true,
+        },
+      });
+    </script>
+
+
+  </body>
+</html>
